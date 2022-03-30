542bd0e52a8be91c5c4e26a6b071bf6b38494c9d
Yann Achard <achardy@microsoft.com>
2022-03-30 15:54:21 -0700

    ReviveSet ↷ Delete

--- packages/dds/tree/src/test/rebase.spec.ts
+++ packages/dds/tree/src/test/rebase.spec.ts
@@ -886,6 +886,132 @@ describe(rebase.name, () => {
 				assert.deepEqual(actual.frames, e2p.frames);
 			});
 		});
+		describe("ReviveSet ↷ Delete", () => {
+			const e1: S.Transaction = {
+				ref: 0,
+				seq: 1,
+				frames: [{
+					marks: [{
+						modify: {
+							foo: [
+								1,
+								{ type: "Delete", length: 3 },
+							],
+						},
+					}],
+				}],
+			};
+			it("base before new", () => {
+				const e2: S.Transaction = {
+					seq: 2,
+					ref: 0,
+					newRef: 1,
+					frames: [{
+						marks: [{
+							modify: {
+								foo: [
+									1,
+									{ type: "ReviveSet", seq: 0, length: 3 },
+									{ type: "Insert", content: [] },
+								],
+							},
+						}],
+					}],
+				};
+				const e2p: S.Transaction = {
+					seq: 2,
+					ref: 0,
+					newRef: 1,
+					frames: [{
+						marks: [{
+							modify: {
+								foo: [
+									1,
+									{ type: "PriorDetach", seq: 1, length: 3 },
+									{ type: "Insert", content: [] },
+								],
+							},
+						}],
+					}],
+				};
+				const actual = rebase(e2, e1);
+				assert.deepEqual(actual.frames, e2p.frames);
+			});
+			it("new before base", () => {
+				const e2: S.Transaction = {
+					seq: 2,
+					ref: 0,
+					newRef: 1,
+					frames: [{
+						marks: [{
+							modify: {
+								foo: [
+									1,
+									{ type: "Insert", content: [] },
+									{ type: "ReviveSet", seq: 0, length: 3 },
+								],
+							},
+						}],
+					}],
+				};
+				const e2p: S.Transaction = {
+					seq: 2,
+					ref: 0,
+					newRef: 1,
+					frames: [{
+						marks: [{
+							modify: {
+								foo: [
+									1,
+									{ type: "Insert", content: [] },
+									{ type: "PriorDetach", seq: 1, length: 3 },
+								],
+							},
+						}],
+					}],
+				};
+				const actual = rebase(e2, e1);
+				assert.deepEqual(actual.frames, e2p.frames);
+			});
+			it("new within base", () => {
+				const e2: S.Transaction = {
+					seq: 2,
+					ref: 0,
+					newRef: 1,
+					frames: [{
+						marks: [{
+							modify: {
+								foo: [
+									1,
+									{ type: "ReviveSet", seq: 0, length: 2 },
+									{ type: "Insert", content: [] },
+									{ type: "ReviveSet", seq: 0 },
+								],
+							},
+						}],
+					}],
+				};
+				const e2p: S.Transaction = {
+					seq: 2,
+					ref: 0,
+					newRef: 1,
+					frames: [{
+						marks: [{
+							modify: {
+								foo: [
+									1,
+									{ type: "PriorDetach", seq: 1, length: 2 },
+									{ type: "Insert", content: [] },
+									{ type: "PriorDetach", seq: 1 },
+								],
+							},
+						}],
+					}],
+				};
+				const actual = rebase(e2, e1);
+				assert.deepEqual(actual.frames, e2p.frames);
+			});
+		});
 	});
 
 	describe("Scenarios", () => {
