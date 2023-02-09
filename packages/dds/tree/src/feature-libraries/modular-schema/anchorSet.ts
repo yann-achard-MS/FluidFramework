d5f67f525d6cb9b8c3ddb9691f7b05d6668765a9
Yann Achard <achardy@microsoft.com>
2023-02-08 17:35:12 -0800

    Commented out FieldAnchorSet refactor

--- packages/dds/tree/src/feature-libraries/modular-schema/anchorSet.ts
+++ packages/dds/tree/src/feature-libraries/modular-schema/anchorSet.ts
@@ -12,7 +12,6 @@ export type MapCallback<TIn, TOut> = (data: TIn) => TOut;
 export interface FieldAnchorSet<TKey, TChangeset, TData = undefined> {
 	count(): number;
 	clone(): FieldAnchorSet<TKey, TChangeset, TData>;
-	// update(key: TKey, func: UpdateCallback<TData, TKey>): void;
 	map<TOut>(func: MapCallback<TData, TOut>): FieldAnchorSet<TKey, TChangeset, TOut>;
 	updateAll(func: UpdateCallback<TData, TKey>): void;
 	mergeIn(set: FieldAnchorSet<TKey, TChangeset, TData>, mergeData?: MergeCallback<TData>): void;
@@ -22,6 +21,35 @@ export interface FieldAnchorSet<TKey, TChangeset, TData = undefined> {
 	rebase(over: TaggedChange<TChangeset>, direction: RebaseDirection): void;
 	entries(): IterableIterator<FieldAnchorSetEntry<TData, TKey>>;
 }
+
+// export class SetOf<T = unknown> {
+// 	private constructor() {}
+// 	protected readonly _T?: T;
+// }
+
+// export type AsSetOf<ValueType, T = unknown> = ValueType & SetOf<T>;
+
+// export interface FieldAnchorFuncs<TKey, TChangeset> {
+// 	readonly factory: <TData>() => SetOf<TData>;
+// 	readonly clone: <TData>(set: SetOf<TData>) => SetOf<TData>;
+// 	readonly map: <TIn, TOut>(set: SetOf<TIn>, func: MapCallback<TIn, TOut>) => SetOf<TOut>;
+// 	readonly track: <TData>(
+// 		set: SetOf<TData>,
+// 		key: TKey,
+// 		data: TData,
+// 		mergeData?: MergeCallback<TData>,
+// 	) => void;
+// 	readonly forget: (set: SetOf, key: TKey) => void;
+// 	readonly lookup: <TData>(
+// 		set: SetOf<TData>,
+// 		key: TKey,
+// 	) => FieldAnchorSetEntry<TData, TKey> | undefined;
+// 	readonly rebase: (
+// 		set: SetOf,
+// 		over: TaggedChange<TChangeset>,
+// 		direction: RebaseDirection,
+// 	) => void;
+// }
 
 export interface FieldAnchorSetEntry<TData, TKey> {
 	readonly key: TKey;
